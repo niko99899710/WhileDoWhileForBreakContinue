6c7fa6d2ac56b82dd28ba2867a5ad7814fe86621
karinaid <bagdasarova-k@inbox.ru>
2024-11-16 18:54:10 +0400

    1/2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+    }
+
+    public static void task1() {
+        System.out.println("ЗАДАНИЕ 1");
+        int a = 1;
+        while (a <= 50) {
+            a++;
+            if (a % 5 == 0) {
+                System.out.println(a);
+            }
+        }
+    }
+    public static void task2 () {
+        System.out.println("ЗАДАНИЕ 2");
+        int i, sum;
+        for ( i = 1, sum = 0; i <= 100; i++) {
+            System.out.println(sum);
+            sum +=i;
+        }
     }
 }
